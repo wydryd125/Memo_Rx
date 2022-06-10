f6840eab52f36fcfbb95ac4016e7fd731a46e856
Up-s <57229970+wydryd125@users.noreply.github.com>
2022-06-10 17:47:55 +0900

    ADD ViewController
    - VC를 생성하여 Scene과 연결

---
+++ Memo_Rx/ViewController/MemoListViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MemoListViewController.swift
+//  Memo_Rx
+//
+//  Created by Lee on 2022/06/10.
+//
+
+import UIKit
+import RxCocoa
+import RxSwift
+
+class MemoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
