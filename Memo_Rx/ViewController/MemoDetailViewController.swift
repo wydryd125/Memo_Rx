3e66f8d5267f08c9a68955decc0a19e2a1aeb652
yu kyung <ljj4904@gmail.com>
2022-06-18 19:35:57 +0900

    UPDATE View Model
    - MemoDetailViewModel 구현

--- Memo_Rx/ViewController/MemoDetailViewController.swift
+++ Memo_Rx/ViewController/MemoDetailViewController.swift
@@ -8,10 +8,35 @@
 import UIKit
 
 class MemoDetailViewController: UIViewController, ViewModelBindableType {
-  
-  var viewModel: MemoDetailViewModel!
+
+    @IBOutlet weak var contentTableView: UITableView!
+    @IBOutlet weak var deleteButton: UIBarButtonItem!
+    @IBOutlet weak var editButton: UIBarButtonItem!
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    
+    var viewModel: MemoDetailViewModel!
   
   func bindViewModel() {
+      viewModel.title
+          .drive(navigationItem.rx.title)
+          .disposed(by: rx.disposeBag)
+      
+      viewModel.contents
+          .bind(to: contentTableView.rx.items) { tableView, row, value in
+              switch row {
+              case 0:
+                  let cell = tableView.dequeueReusableCell(withIdentifier: "contentCell")
+                  cell?.textLabel?.text = value
+                 return cell
+              case 1:
+                  let cell = tableView.dequeueReusableCell(withIdentifier: "dateCell")
+                  cell?.textLabel?.text = value
+                  return cell
+              default:
+                  fatalError()
+              }
+          }
+          .disposed(by: rx.disposeBag)
   
   }
 
--- Memo_Rx/ViewModel/MemoDetailViewModel.swift
+++ Memo_Rx/ViewModel/MemoDetailViewModel.swift
@@ -6,7 +6,32 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+import Action
 
-class MemoDetailViewModel {
-  
+class MemoDetailViewModel: CommonViewMoel {
+    let memo: Memo
+    
+    private var formmatter: DateFormatter = {
+        let f = DateFormatter()
+        f.locale = Locale(identifier: "ko_kr")
+        f.dateStyle = .medium
+        f.timeStyle = .medium
+        
+        return f
+    }()
+    
+    var contents: BehaviorSubject<[String]>
+
+    init(memo: Memo, title: String, sceneCoordinator: SceneCoordinatortype, storage: MemoStorageType) {
+        self.memo = memo
+        
+        contents = BehaviorSubject<[String]>(value: [
+            memo.content,
+            formmatter.string(from: memo.insertDate)
+        ])
+        
+        super.init(title: title, sceneCoordinator: sceneCoordinator, storage: storage)
+    }
 }
